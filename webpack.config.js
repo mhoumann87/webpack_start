61aba5f9000dee44e48a0bbe4fef12e6c242885b
Michael Houmann <mhoumann87@gmail.com>
2021-06-30 14:37:48 +0200

    Add webpack and babel config files

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/i,
+        exclude: /node_modules/,
+        use: {
+          // without additional setting, this will reference .babelrc
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+}
